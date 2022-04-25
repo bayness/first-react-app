a205d7c2b1811d09cdc42a6c67268ec1bf068b5f
Younes <YOUNES@192.168.1.113>
2022-04-25 01:27:31 +0000

    first commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
+
+
+function times(a,b){
+  return a*b ;
+}
+
+console.log(times(9,9));
+console.log("sirou t9awdou al3achrane");
